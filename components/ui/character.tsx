a6778d644120e070a87056ccc8d56c3410782afb
Ryan <119005046+CodeByDex@users.noreply.github.com>
2025-01-05 18:57:23 -0500

    updated more styling

--- components/ui/character.tsx
+++ components/ui/character.tsx
@@ -14,7 +14,7 @@ interface ReadingComponentProps {
 export function ReadingComponent({ reading }: ReadingComponentProps) {
   return (
     <div className='flex'>
-      <div className='flex items-center justify-center p-2'>
+      <div className='flex items-center justify-center p-2 whitespace-nowrap'>
         {reading.id}
       </div>
       <div className='p-2'>
@@ -30,10 +30,10 @@ export function ReadingComponent({ reading }: ReadingComponentProps) {
 export default function CharacterComponent({ character }: CharacterComponentProps) {
   return (
     <div className='flex'>
-      <div className='flex items-center justify-center p-4 text-9xl'>
+      <div className='flex items-center justify-center p-4 text-9xl w-1/3'>
         {character.id}
       </div>
-      <div className='p-4'>
+      <div className='p-4 w-2/3'>
         <p><strong>Type:</strong> {character.type}</p>
         <p><strong>Readings:</strong></p>
         <p>{character.readings.map(x => (
@@ -42,11 +42,11 @@ export default function CharacterComponent({ character }: CharacterComponentProp
         <p><strong>Radical:</strong> {character.radical.join(', ')}</p>
         <p><strong>Stroke Count:</strong> {character.stroke}</p>
         {character.meaning && <p><strong>Meaning:</strong> {character.meaning}</p>}
-        {character.mnemonic && <p><strong>Mnemonic:</strong> {character.mnemonic}</p>}
-        <p><strong>Note:</strong> {character.note}</p>
-        <p><strong>JLPT Level:</strong> N{character.JLPT}</p>
+        <p><strong>JLPT:</strong> N{character.JLPT}</p>
         <p><strong>Grade:</strong> {character.grade}</p>
-        <p><strong>Advanced Placement:</strong> {character.AP ? 'Yes' : 'No'}</p>
+        <p><strong>AP:</strong> {character.AP ? 'Yes' : 'No'}</p>
+        {character.mnemonic && <p><strong>Mnemonic:</strong> {character.mnemonic}</p>}
+        {character.note && <p><strong>Note:</strong> {character.note}</p> }
         <p><strong>Examples:</strong></p>
         <p>{character.examples.map(x => (
           <ReadingComponent reading={x} key={x.id} />
