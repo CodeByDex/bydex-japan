93f16f961fddf6536ad877fa39098c24dfce63b7
Ryan <119005046+CodeByDex@users.noreply.github.com>
2025-01-07 20:24:46 -0500

    refactored character card to use reusable components

--- components/ui/character.tsx
+++ components/ui/character.tsx
@@ -1,27 +1,19 @@
 'use client';
 
 import React from 'react';
-import {useState} from 'react'
+import { useState } from 'react'
 import { character, reading } from '@lib/definitions';
 
-interface CharacterProps {
-  character: character;
-}
-
-interface ReadingComponentProps {
-  reading: reading
-}
-
-export function ReadingComponent({ reading }: ReadingComponentProps) {
+export function ReadingComponent({ reading }: {reading: reading}) {
   const [isExpanded, setIsExpanded] = useState(false);
 
   const toggleExpand = () => {
     setIsExpanded(!isExpanded);
   };
-  
+
   return (
     <div className='flex'>
-      <div 
+      <div
         className='flex items-center justify-center p-2 whitespace-nowrap'
         onClick={toggleExpand}
       >
@@ -39,31 +31,41 @@ export function ReadingComponent({ reading }: ReadingComponentProps) {
   )
 }
 
-export default function CharacterFullInfo({ character }: CharacterProps) {
+export default function CharacterFullInfo({ character }: {character: character}) {
   return (
     <div className='flex'>
-      <div className='flex items-center justify-center p-4 text-8xl w-1/3'>
-        {character.id}
-      </div>
-      <div className='p-4 w-2/3'>
-        <p><strong>Type:</strong> {character.type}</p>
-        <p><strong>Readings:</strong></p>
-        {character.readings.map(x => (
-          <ReadingComponent reading={x} key={x.id} />
-        ))}
-        <p><strong>Radical:</strong> {character.radical.join(', ')}</p>
-        <p><strong>Stroke Count:</strong> {character.stroke}</p>
-        {character.meaning && <p><strong>Meaning:</strong> {character.meaning}</p>}
-        <p><strong>JLPT:</strong> N{character.JLPT}</p>
-        <p><strong>Grade:</strong> {character.grade}</p>
-        <p><strong>AP:</strong> {character.AP ? 'Yes' : 'No'}</p>
-        {character.mnemonic && <p><strong>Mnemonic:</strong> {character.mnemonic}</p>}
-        {character.note && <p><strong>Note:</strong> {character.note}</p> }
-        <p><strong>Examples:</strong></p>
-        {character.examples.map(x => (
-          <ReadingComponent reading={x} key={x.id} />
-        ))}      
-      </div>
+      <LargeCharacter className="flex p-4 w-1/3" char={character.id} />
+      <CharacterInfo className='p-4 w-2/3' character={character} />
     </div >
   );
+}
+
+export function LargeCharacter({ char, className }: { char: string, className: string }) {
+  return <div className={`items-center justify-center text-8xl ${className}`}>
+    {char}
+  </div>
+}
+
+export function CharacterInfo({ character, className }: { character: character, className: string }) {
+  return (
+    <div className={className}>
+      <p><strong>Type:</strong> {character.type}</p>
+      <p><strong>Readings:</strong></p>
+      {character.readings.map(x => (
+        <ReadingComponent reading={x} key={x.id} />
+      ))}
+      <p><strong>Radical:</strong> {character.radical.join(', ')}</p>
+      <p><strong>Stroke Count:</strong> {character.stroke}</p>
+      {character.meaning && <p><strong>Meaning:</strong> {character.meaning}</p>}
+      <p><strong>JLPT:</strong> N{character.JLPT}</p>
+      <p><strong>Grade:</strong> {character.grade}</p>
+      <p><strong>AP:</strong> {character.AP ? 'Yes' : 'No'}</p>
+      {character.mnemonic && <p><strong>Mnemonic:</strong> {character.mnemonic}</p>}
+      {character.note && <p><strong>Note:</strong> {character.note}</p>}
+      <p><strong>Examples:</strong></p>
+      {character.examples.map(x => (
+        <ReadingComponent reading={x} key={x.id} />
+      ))}
+    </div>
+  )
 }
