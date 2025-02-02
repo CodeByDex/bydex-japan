7ae1b3be26c24ff9038dc8a582a7e8a13150f46e
Ryan <119005046+CodeByDex@users.noreply.github.com>
2025-02-02 14:42:34 -0500

    Feature nav sub (#43)
    * fixed conditional nav styling for current page
    
    * initial working sub nav menu
    
    * updated styling for sub pages on the quiz nav

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,43 +1,63 @@
 "use client"
 
-// CLSX conditional styling is not working
 import clsx from 'clsx';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { useState } from 'react';
 
 export function NavBar() {
     const pathName = usePathname();
+    const [navFocus, setNavFocus] = useState<string | null>(null);
+
     return (
         <nav className='flex'>
-            <Link
-                href={"/"}
-                className={clsx(
-                    '',
-                    {
-                        'text-blue-600': pathName === "."
-                    }
-                )}            >
-                Kanado
-            </Link>
-            <Link
-                href={"/character"}
-                className={clsx(
-                    '',
-                    {
-                        'text-blue-600': pathName === "./character"
-                    }
-                )}>
-                Characters
-            </Link>
-            <Link href={"/quiz"}
-                className={clsx(
-                    '',
-                    {
-                        'text-blue-600': pathName === "./character"
-                    }
-                )}>
-                Quiz
-            </Link>
+            <div>
+                <Link
+                    href={"/"}
+                    className={clsx('', { 'hover-bg-color': pathName === "/" })}>
+                    Kanado
+                </Link>
+            </div>
+            <div
+                onMouseEnter={() => setNavFocus("character")}
+                onMouseLeave={() => setNavFocus(null)}
+            >
+                <Link
+                    href={"/character"}
+                    className={clsx('', { 'hover-bg-color': pathName === "/character" })}>
+                    Characters
+                </Link>
+                {/* {navFocus === "character" && (
+                    <div className='mt-4'>
+                        <Link href={"/character/kana"}>Kana</Link>
+                    </div>
+                )} */}
+            </div>
+            <div
+                onMouseEnter={() => setNavFocus("quiz")}
+                onMouseLeave={() => setNavFocus(null)}
+            >
+                <Link
+                    href={"/quiz"}
+                    className={clsx('', { 'hover-bg-color': pathName.indexOf("/quiz") === 0 })}>
+                    Quiz
+                </Link>
+                {navFocus === "quiz" && (
+                    <div className='mt-4'>
+                        <Link href={"/quiz/jst"}>Japan Study Tour</Link>
+                        <Link href={"/quiz/katakana"}>Katakana</Link>
+                        <Link href={"/quiz/hiragana"}>Hiragana</Link>
+                        <Link href={"/quiz/kanji"}>Kanji</Link>
+                        <Link href={"/quiz/n1"}>N1</Link>
+                        <Link href={"/quiz/n2"}>N2</Link>
+                        <Link href={"/quiz/n3"}>N3</Link>
+                        <Link href={"/quiz/n4"}>N4</Link>
+                        <Link href={"/quiz/n5"}>N5</Link>
+                        <Link href={"/quiz/ap"}>AP</Link>
+                    </div>
+                )}
+            </div>
+
         </nav>
     )
 }
